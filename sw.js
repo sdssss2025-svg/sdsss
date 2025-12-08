9a4fc084ba9b35030513ef118281c8d7502d2201
sdssss2025-svg <sdssss2025@gmail.com>
2025-12-09 00:31:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,21 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("sastha-cache").then((cache) => {
+      return cache.addAll([
+        "/sdsss/",
+        "/sdsss/index.html",
+        "/sdsss/icon-192.png",
+        "/sdsss/icon-512.png",
+        "/sdsss/ayyappa.jpg"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
